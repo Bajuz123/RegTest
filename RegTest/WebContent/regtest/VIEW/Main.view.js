984037ba125fb57ae50bf606827cd3cad0114dfc
Bajuz123 <stoma1234@gmail.com>
2018-09-18 08:47:06 +0200

    Main adapt

--- RegTest/WebContent/regtest/VIEW/Main.view.js
+++ RegTest/WebContent/regtest/VIEW/Main.view.js
@@ -1,22 +1,28 @@
 sap.ui.jsview("regtest.VIEW.Main", {
 
-	/** Specifies the Controller belonging to this View. 
-	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
-	* @memberOf regtest.Main
-	*/ 
+	/**
+	 * Specifies the Controller belonging to this View. In the case that it is
+	 * not implemented, or that "null" is returned, this View does not have a
+	 * Controller.
+	 * 
+	 * @memberOf regtest.Main
+	 */
 	getControllerName : function() {
 		return "regtest.CONTROLLER.Main";
 	},
 
-	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
-	* Since the Controller is given to this method, its event handlers can be attached right away. 
-	* @memberOf regtest.Main
-	*/ 
+	/**
+	 * Is initially called once after the Controller has been instantiated. It
+	 * is the place where the UI is constructed. Since the Controller is given
+	 * to this method, its event handlers can be attached right away.
+	 * 
+	 * @memberOf regtest.Main
+	 */
 	createContent : function(oController) {
- 		return new sap.m.Page({
-			title: "Autotest",
-			content: [
-			
+		return new sap.m.Page({
+			title : "Autotest",
+			content : [
+
 			]
 		});
 	}
