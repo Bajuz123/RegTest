d518a8ca873fe3a517da6608a7fdc3828b19c45f
Bajuz123 <stoma1234@gmail.com>
2018-09-18 15:09:30 +0200

    RegTestDetail

---
+++ RegTest/WebContent/regtest/VIEW/RegTestDetail.view.js
@@ -0,0 +1,37 @@
+sap.ui.jsview("regtest.VIEW.RegTestDetail", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf regtest.RegTestDetail
+	*/ 
+	getControllerName : function() {
+		return "regtest.CONTROLLER.RegTestDetail";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf regtest.RegTestDetail
+	*/ 
+	createContent : function(oController) {
+//panel 
+//		Input
+//		TextView
+		//Button "Add" press="onAddClick"
+		
+		var btnAdd = sap.m.Button("btnAdd", {
+			
+//			press = "onAddClick"
+		});		
+		
+		var fieldName = sap.m.Input("fldName");
+		var areaXML   = sap.m.TextArea("areaXML");
+		
+		return new sap.m.Page({
+			title: "RegTest Detail",
+			content: [
+			  btnAdd, fieldName, areaXML
+			]
+		});
+	}
+
+});
