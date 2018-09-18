4f8588d5505729fe978798a4d0e3a805e3eda741
Bajuz123 <stoma1234@gmail.com>
2018-09-18 13:44:42 +0200

    SAP Data

--- RegTest/WebContent/regtest/CONTROLLER/Main.controller.js
+++ RegTest/WebContent/regtest/CONTROLLER/Main.controller.js
@@ -9,18 +9,28 @@ sap.ui.controller("regtest.CONTROLLER.Main", {
 	 * @memberOf regtest.Main
 	 */
 	onInit : function() {
-		var oModel = new sap.ui.model.json.JSONModel();
+		//JSON Data
+/*		var oModel = new sap.ui.model.json.JSONModel();
 		oModel.loadData("regtest/JSON/RegTest_DATA.json");
 		this.getView().setModel(oModel);
-
-		/*
-		 * var oModel = new sap.ui.model.odata.ODataModel(this
-		 * .getUrl("/sap/opu/odata/sap/Z_REG_TEST_SRV"), true, "stoma",
-		 * "palipali89"); this.getView().setModel(oModel);
-		 * this.getView().bindElement("/REG_TEST_SET");
-		 */
-
+*/
+	
+		//SAP Data
+		  var oModel = new sap.ui.model.odata.ODataModel(this
+		  .getUrl("/sap/opu/odata/sap/Z_REG_TEST_SRV"), true, "stoma", "palipali89"); 
+		  this.getView().setModel(oModel);
 	},
+	
+	getUrl : function(sUrl) {
+		if (sUrl == "")
+			return sUrl;
+		if (window.location.hostname == "localhost") {
+			return "proxy" + sUrl;
+		} else {
+			return sUrl;
+		}
+	},
+	
 /**
  * Similar to onAfterRendering, but this hook is invoked before the controller's
  * View is re-rendered (NOT before the first rendering! onInit() is used for
