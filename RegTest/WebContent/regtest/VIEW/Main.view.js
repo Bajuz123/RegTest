7fb46cff7547b82a4a20f1f8888c91c09e1268d8
Bajuz123 <stoma1234@gmail.com>
2018-09-18 08:39:49 +0200

    First Commit

---
+++ RegTest/WebContent/regtest/VIEW/Main.view.js
@@ -0,0 +1,24 @@
+sap.ui.jsview("regtest.VIEW.Main", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf regtest.Main
+	*/ 
+	getControllerName : function() {
+		return "regtest.CONTROLLER.Main";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf regtest.Main
+	*/ 
+	createContent : function(oController) {
+ 		return new sap.m.Page({
+			title: "Autotest",
+			content: [
+			
+			]
+		});
+	}
+
+});
