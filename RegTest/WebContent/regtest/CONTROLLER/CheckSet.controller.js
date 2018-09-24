6fe01f5c788bb09bfda2ac2e04415a8fa832da10
Martin Pala <Martin Pala@DESKTOP-8ILARQC>
2018-09-24 16:54:47 +0200

    CheckSet

--- RegTest/WebContent/regtest/CONTROLLER/CheckSet.controller.js
+++ RegTest/WebContent/regtest/CONTROLLER/CheckSet.controller.js
@@ -21,7 +21,7 @@ sap.ui.controller("regtest.CONTROLLER.CheckSet", {
 			var cells = rows[selIndex].getCells();
 			var idCheckSet = cells[0].getValue();
 			var oModelCheckSet = sap.ui.getCore().getModel();
-			oModelCheckSet.remove("/REG_TEST_SET(id_reg_test='" + idCheckSet
+			oModelCheckSet.remove("/REG_CHECK_SET_SET(id_check_set='" + idCheckSet
 					+ "')", {
 				method : "DELETE",
 				success : function(data) {
