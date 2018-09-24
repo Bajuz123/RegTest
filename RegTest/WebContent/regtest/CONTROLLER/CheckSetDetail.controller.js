c6221d85998fe7eb3295fa908ddbf2c2dc24e721
JBreza <JBreza@LAPTOP-S8J6RRN0>
2018-09-24 13:06:20 +0200

    Commit CheckSetDetail controller/view 2 pokus

---
+++ RegTest/WebContent/regtest/CONTROLLER/CheckSetDetail.controller.js
@@ -0,0 +1,155 @@
+sap.ui.controller("regtest.CONTROLLER.CheckSetDetail", {
+
+/**
+* Called when a controller is instantiated and its View controls (if available) are already created.
+* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+* @memberOf regtest.RegTestDetail
+*/
+	onInit: function() {
+	},
+/*
+	onBackRegClick: function() {
+		oSplitApp.toDetail("idRegTest1");				
+		sap.ui.getCore().byId("fldIDReg").setValue("");		
+	}, */
+	
+	onOKCheckClick: function() {
+		var oEntry = {};		
+		
+//escape the texts first!!! sap.ui functionality
+//		oEntry.id_check_set = sap.ui.getCore().byId("fldIDCheck").getValue();
+//		oEntry.Name = sap.ui.getCore().byId("fldName").getValue();
+//		oEntry.Implementation_class = sap.ui.getCore().byId("fldImplClass").getValue(); 
+	}
+	});
+		/*
+		var oModelRegTest = sap.ui.getCore().getModel();
+
+		if ( oEntry.id_reg_test == '' ) { //insert
+			oModelRegTest.create("/REG_TEST_SET", oEntry);
+			sap.m.MessageToast.show("Add successfull");
+			oSplitApp.toDetail("idRegTest1");		
+		} else { //update
+			debugger;			
+			oModelRegTest.update("/REG_TEST_SET(id_reg_test='" + oEntry.id_reg_test + "')", oEntry, {
+				success : function(data) {
+					sap.m.MessageToast.show("Update successfull");
+				},
+				error : function(e) {
+					sap.m.MessageToast.show("Update error");
+				}				
+			});
+			oModelRegTest.refresh();
+			oSplitApp.toDetail("idRegTest1");		
+		}
+		sap.ui.getCore().byId("fldIDReg").setValue("");		
+	},
+	
+    onRunClick : function() {
+//		update/insert
+//    	start web service
+    },		
+
+    onPlaceholderClick : function() {
+		sap.ui.getCore().byId("idListRelatedCheck").setVisible(false);
+		sap.ui.getCore().byId("idListRelatedPlace").setVisible(true);
+    },		
+
+    onCheckSetClick : function() {
+		sap.ui.getCore().byId("idListRelatedCheck").setVisible(true);
+		sap.ui.getCore().byId("idListRelatedPlace").setVisible(false);
+    },
+    
+    onAddCheckClick: function(){   	
+	   
+    },	
+
+    onDelCheckClick: function() {
+    	
+    },		
+
+    onEditCheckClick: function() {
+    	
+    },		
+
+	onAddPlaceClick: function() {
+		this._oDialog = sap.ui.xmlfragment("com.tutorial.fragments.addDialog",this);
+	     this._oDialog.open(); 
+		// this._getDialog().open();
+	},		
+
+	onDelPlaceClick: function() {
+		this._oDialog = sap.ui.xmlfragment("com.tutorial.fragments.delDialog",this);
+	     this._oDialog.open(); 
+	},		
+
+	onEditPlaceClick: function() {
+		this._oDialog = sap.ui.xmlfragment("com.tutorial.fragments.updDialog",this);
+	     this._oDialog.open(); 
+	},		
+	onCloseDialog : function () {
+        this._getDialog().close();
+     },
+	 _getDialog : function () {
+         // create dialog lazily
+         if (!this._oDialog) {
+            // create dialog via fragment factory
+            this._oDialog = sap.ui.xmlfragment("com.tutorial.fragments.addDialog", this);
+            // connect dialog to view (models, lifecycle)
+            this.getView().addDependent(this._oDialog);
+         }
+         return this._oDialog;
+      },
+
+	/* FRAGMENT
+	 onOpenDialog : function () {
+         this._getDialog().open();
+      },
+	onCloseDialog : function () {
+        this._getDialog().close();
+     },
+     _getDialog : function () {
+         // create dialog lazily
+         if (!this._oDialog) {
+            // create dialog via fragment factory
+            this._oDialog = sap.ui.xmlfragment("com.tutorial.fragments.addDialog", this);
+            // connect dialog to view (models, lifecycle)
+            this.getView().addDependent(this._oDialog);
+         }
+         return this._oDialog;
+      }*/
+	
+		
+/**
+* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+* (NOT before the first rendering! onInit() is used for that one!).
+* @memberOf regtest.RegTestDetail
+*/
+/*
+	onBeforeRendering: function() {
+		debugger;
+		var id_reg_test = sap.ui.getCore().byId("fldIDReg").getValue();
+		if (id_reg_test != '') {
+			reloadModel(oUser);			
+			var oPlaceTable = sap.ui.getCore().byId("idPlaceTable");
+//			oPlaceTable.bindRows("/REG_PLACE_SET(id_reg_test='" + sap.ui.getCore().byId("fldIDReg").getValue() + "')");			
+		}	
+	},
+*/
+/**
+* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+* This hook is the same one that SAPUI5 controls get after being rendered.
+* @memberOf regtest.RegTestDetail
+*/
+//	onAfterRendering: function() {
+//
+//	},
+
+/**
+* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+* @memberOf regtest.RegTestDetail
+*/
+//	onExit: function() {
+//	}
+/*
+});*/
---
+++ RegTest/WebContent/regtest/VIEW/CheckSetDetail.view.js
@@ -0,0 +1,200 @@
+sap.ui.jsview("regtest.VIEW.CheckSetDetail", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf regtest.RegTestDetail
+	*/ 
+	getControllerName : function() {
+		return "regtest.CONTROLLER.CheckSetDetail";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf regtest.RegTestDetail
+	*/ 
+	createContent : function(oController) {	
+		var btnOKCheck = new sap.m.Button("btnOKCheck", {
+			text : "OK",
+			icon : "sap-icon://save",
+		    press:  oController.onOKCheckClick   });		
+
+/*		var btnBackReg = new sap.m.Button("btnBackReg", {
+			text : "Back",
+			icon : "sap-icon://undo",
+		    press:  oController.onBackRegClick   });		
+
+		var btnRun = new sap.m.Button("btnRun", {
+			text : "Run RegTest",
+			icon : "sap-icon://process",
+		    press:  oController.onRunClick   });		
+
+		var btnRegPlace = new sap.m.Button("btnRegPlace", {
+			text : "Placeholders",
+			icon : "sap-icon://x-ray",
+		    press:  oController.onPlaceholderClick   });		
+
+		var btnCheckSet = new sap.m.Button("btnSet", {
+			text : "CheckSets",
+			icon : "sap-icon://stethoscope",
+		    press:  oController.onCheckSetClick   }); 	
+	
+		var fieldIDCheck = new sap.m.Input("fldIDCheck", {visible: false}); 
+
+		var checkTestNameLabel = new sap.m.Label("idcheckTestName", {text: "CheckSetName"});
+		var fieldName = new sap.m.Input("fldName");
+		var panelcheckDetailName = new sap.m.Panel("idPanelCheckDetailName", {
+			content : [
+			           checkTestNameLabel, fieldName           
+			          ]
+		});
+		/*
+		var regTestXMLLabel = new sap.m.Label("idRegTestXML", {text: "XML"});
+		var areaXML   = new sap.m.TextArea("areaXML", {
+			width: "100%",
+			growing: true
+		});
+		
+		var fileReader = new sap.ui.unified.FileUploader({ 
+	          uploadUrl : "",
+	          buttonText : "Upload XML",  
+	          fileType : "xml",  
+	          maximumFileSize : 1,
+	          buttonOnly : true,
+	          icon : "sap-icon://upload",
+	          change : function(e){
+	          var file = e.getParameter("files") && e.getParameter("files")[0];
+	           if(file && window.FileReader){  
+	              var reader = new FileReader();  
+	              var that = this;  
+	              reader.onload = function(evn) { 
+	            	  debugger;
+	                var strCSV= evn.target.result; //string in CSV
+	        		sap.ui.getCore().byId("areaXML").setValue(strCSV);
+                  };
+	              reader.readAsText(file);  
+	            }
+	            }
+	        });
+
+		var panelRegDetailXML = new sap.m.Panel("idPanelRegDetailXML", {
+			content : [
+			           regTestXMLLabel, areaXML           
+			          ]
+		});
+
+		var panelRegDetailXMLButton = new sap.m.Panel("idPanelRegDetailXMLButton", {
+			content : [
+			           fileReader           
+			          ]
+		});
+
+		var oPlaceTable = new sap.ui.table.Table({
+			tableID : "idPlaceTable",
+			visibleRowCount : 5,
+			selectionMode: sap.ui.table.SelectionMode.Single,
+			editable : true
+		});
+
+		oPlaceTable.addColumn(new sap.ui.table.Column({
+			label: new sap.ui.commons.Label({text: "ID"}),
+			template: new sap.ui.commons.TextField().bindProperty("value","id_reg_test"),
+			visible: true
+		} ));
+
+		oPlaceTable.addColumn(new sap.ui.table.Column({
+			label: new sap.ui.commons.Label({text: "Placeholder"}),
+			template: new sap.ui.commons.TextField().bindProperty("value","placeholder"),
+			visible: true
+		} ));
+
+		oPlaceTable.addColumn(new sap.ui.table.Column({
+			label: new sap.ui.commons.Label({text: "Replace with"}),
+			template: new sap.ui.commons.TextField().bindProperty("value","replace_with"),
+			visible: true
+		} ));
+		
+		var oCheckTable = new sap.ui.table.Table({
+			tableID : "idCheckTable",
+			visibleRowCount : 5,
+			selectionMode: sap.ui.table.SelectionMode.Single,
+			editable : true
+		});
+
+		oCheckTable.addColumn(new sap.ui.table.Column({
+			label: new sap.ui.commons.Label({text: "Check Set"}),
+			template: new sap.ui.commons.TextField().bindProperty("value","id_check_set"),
+			visible: true
+		} ));
+
+		oCheckTable.addColumn(new sap.ui.table.Column({
+			label: new sap.ui.commons.Label({text: "Running nr"}),
+			template: new sap.ui.commons.TextField().bindProperty("value","running_nr"),
+			visible: true
+		} ));
+
+		oPlaceTable.bindRows("/REG_PLACE_SET");			
+		oCheckTable.bindRows("/REG_SET");
+
+		var btnPlaceAdd = new sap.m.Button("btnPlaceAdd", {
+			text : "Add",
+			icon : "sap-icon://add",
+		    press:  oController.onAddPlaceClick   });		
+
+		var btnPlaceDel = new sap.m.Button("btnPlaceDel", {
+			text : "Delete",
+			icon : "sap-icon://delete",
+		    press:  oController.onDelPlaceClick   });		
+
+		var btnPlaceEdit = new sap.m.Button("btnPlaceEdit", {
+			text : "Edit",
+			icon : "sap-icon://edit",
+		    press:  oController.onEditPlaceClick   });		
+
+		var panelRegDetail = new sap.m.Panel("idPanelRegDetail", {
+			content : [
+			           btnOKReg, btnBackReg, btnRun, fieldIDReg, 
+			           panelRegDetailName,
+			           panelRegDetailXML,
+			           panelRegDetailXMLButton,
+			           btnRegPlace, btnCheckSet 
+			           ]
+		});		
+
+		oCheckTable.visible = false;
+		var panelRelatedPlace = new sap.m.Panel("idListRelatedPlace", {
+			content : [
+			           btnPlaceAdd, btnPlaceDel, btnPlaceEdit, oPlaceTable
+			           ]
+		});		
+
+		var btnCheckAdd = new sap.m.Button("btnCheckAdd", {
+			text : "+",
+//			icon : "sap-icon://add",
+		    press:  oController.onAddCheckClick   });		
+
+		var btnCheckDel = new sap.m.Button("btnCheckDel", {
+			text : "-",
+//			icon : "sap-icon://add",
+		    press:  oController.onDelCheckClick   });		
+
+		var btnCheckEdit = new sap.m.Button("btnCheckEdit", {
+			text : "=>",
+//			icon : "sap-icon://add",
+		    press:  oController.onEditCheckClick   });		
+		
+		var panelRelatedCheck = new sap.m.Panel("idListRelatedCheck", {
+			content : [
+			          btnCheckAdd, btnCheckDel, btnCheckEdit, oCheckTable
+			          ]
+		});		
+
+		sap.ui.getCore().byId("idListRelatedCheck").setVisible(false);
+	*/	
+		return new sap.m.Page({
+			title: "RegTest Detail",
+			content: [ btnOKCheck
+		//	  panelRegDetail, panelRelatedPlace, panelRelatedCheck 
+			]
+		});
+	}
+});
